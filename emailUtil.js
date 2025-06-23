34ebc5d08a5846adf56b4f1797e3aa9007619a80
GenghisKhan-creator <123103694+GenghisKhan-creator@users.noreply.github.com>
2025-06-23 21:09:34 +0000

    let's see

--- emailUtil.js
+++ emailUtil.js
@@ -5,11 +5,7 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 const db = new pg.Client({
-  user: process.env.POSTGRES_USER,
-  host: process.env.POSTGRES_HOST,
-  database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PASSWORD,
-  port: process.env.POSTGRES_PORT,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
         rejectUnauthorized: false
     }
--- index.js
+++ index.js
@@ -37,11 +37,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 const db = new pg.Client({
-    user: process.env.POSTGRES_USER,
-    host: process.env.POSTGRES_HOST,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    port: process.env.POSTGRES_PORT,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }
